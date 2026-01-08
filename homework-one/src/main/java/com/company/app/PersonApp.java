da9eb0fd99e4ace0acb8c510fdc98c5caf11f744
Evgeniyyd <brai.kel@icloud.com>
2026-01-08 11:26:11 +0400

    добавил проверку на null

--- homework-one/src/main/java/com/company/app/PersonApp.java
+++ homework-one/src/main/java/com/company/app/PersonApp.java
@@ -20,22 +20,19 @@ public class PersonApp {
 
     // - Статический метод, который вернет затраты компании(зарплата всех сотрудников).
     public static double getSumSalary(Employee[] employees) {
-        double sum = Arrays.stream(employees).mapToDouble(Employee::calculateSalary).sum();
+        double sum = Arrays.stream(employees).filter(Objects::nonNull).mapToDouble(Employee::calculateSalary).sum();
         return sum;
     }
 
     // - Статический метод, который вернет доход компании(все продажи).
     // Это приносят только **SalesManagers**.
     public static double sumCompanyRevenue(Employee[] employees) {
-        double sum = Arrays.stream(employees).filter(employee -> employee instanceof SaleManager).map(employee ->
+        double sum = Arrays.stream(employees).filter(Objects::nonNull).filter(employee -> employee instanceof SaleManager).map(employee ->
                 (SaleManager) employee).mapToDouble(SaleManager::getTotalSales).sum();
         return sum;
     }
 
     public static boolean employeeSearch(Employee[] employees, Employee employee) {
-        if (employees == null || employees.length == 0) {
-            return false;
-        }
-      return   Arrays.asList(employees).contains(employee);
+      return   Arrays.stream(employees).filter(Objects::nonNull).anyMatch(employee1 -> Objects.equals(employee1,employee));
     }
 }
