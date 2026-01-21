11101028c1e9a8faea8e10c0f48801778288bd15
Evgeniyyd <brai.kel@icloud.com>
2026-01-21 19:04:37 +0300

    реализовал первые два метода и подключил библиотеку lombok и maven к проекту

--- homework-two/src/Main.java
+++ homework-two/src/Main.java
@@ -1,5 +1,42 @@
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+import java.util.stream.Stream;
+
+@EqualsAndHashCode()
+@ToString(callSuper = true)
 public class Main {
     public static void main(String[] args) {
+        printStringReverse("khjg");
+        isPhoneNumber("89999797793");
+        printSubStringReverse("njbh", 0, 1);
 
+    }
+
+    public static void printStringReverse(@NonNull String reverse) {
+        if (reverse.isEmpty() || reverse.equals(" ")) {
+            System.out.println("Wrong string");
+
+        }
+        StringBuilder stringBuilder = new StringBuilder(reverse);
+        Stream.of(stringBuilder)
+                .map(stringBuilder1 -> stringBuilder1.reverse())
+                .filter(revers -> !revers.isEmpty() || !revers.equals(" "))
+                .forEach(System.out::println);
+    }
+
+    public static void isPhoneNumber(@NonNull String isNaber) {
+        if (isNaber.isEmpty() || isNaber.equals(" ")){
+            System.out.println((String) null);
+
+        }
+        System.out.println(Stream.of(isNaber)
+                .anyMatch(namer -> namer.startsWith("8") &&
+                        namer.length() == 11 && namer.matches("\\d+")));
+
+    }
+
+    public static void printSubStringReverse(String string, int start, int finish) {
+        StringBuilder stringBuilder = new StringBuilder(string);
     }
 }
