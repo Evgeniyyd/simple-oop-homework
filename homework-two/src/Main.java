fa7056655ecfe2dbbcca112323b3b334fe924335
Evgeniyyd <brai.kel@icloud.com>
2026-01-27 11:48:06 +0300

    доработал методы переписав их и оптимизировав в фаил pom.xml  повысил версию джавы до 21

--- homework-two/src/Main.java
+++ homework-two/src/Main.java
@@ -1,6 +1,8 @@
 import lombok.NonNull;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -9,61 +11,45 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         printStringReverse("khjg");
-        isPhoneNumber("89999797793");
+        System.out.println(isPhoneNumber("89999798834"));
         printSubStringReverse("мапана", 0, 2);
-        getWordsReverseInColumn("Hello my nice world");
-        getWordsReverse("Hello my nice world");
+        System.out.println(getWordsReverse("Hello my nice world"));
+        System.out.println(getWordsReverseInColumn("Hello my nice world"));
     }
 
     public static void printStringReverse(@NonNull String string) {
         if (string.isEmpty() || string.equals(" ")) {
             System.out.println("Wrong string");
         }
-        char[] charArray = string.toCharArray();
-        IntStream.rangeClosed(1, charArray.length)
-                .mapToObj(i -> charArray[charArray.length - i])
-                .forEach(System.out::println);
-//        StringBuilder stringBuilder = new StringBuilder(string);
-//        System.out.println(stringBuilder.reverse());
-//взять строку, разбить её на символы из этой строки создать интстрим ,
-// используя массив и  интстрим запускать чисела  и обращаться к символам с конца
-
-//        Stream.of(stringBuilder)
-//                .map(stringBuilder1 -> stringBuilder1.reverse())
-//                .forEach(System.out::println);
+        StringBuilder stringBuilder = new StringBuilder(string);
+        System.out.println(stringBuilder.reverse());
     }
 
-    public static void isPhoneNumber(@NonNull String isNaber) {
-        if (isNaber.isEmpty() || isNaber.equals(" ")) {
+    public static Boolean isPhoneNumber(@NonNull String number) {
+        if (number.isEmpty() || number.equals(" ")) {
             System.out.println((String) null);
         }
-        System.out.println(Stream.of(isNaber)
-                .anyMatch(namer -> namer.startsWith("8") &&
-                        namer.length() == 11 && namer.matches("\\d+")));
-
+        return number.matches("^8\\d{10}");
     }
 
     public static void printSubStringReverse(String string, int start, int finish) {
-        StringBuilder stringBuilder = new StringBuilder(string);
-        stringBuilder.setCharAt(start, string.charAt(finish));
-        stringBuilder.setCharAt(finish, string.charAt(start));
-        System.out.println(stringBuilder);
+        String substring = string.substring(start, finish + 1);
+        StringBuilder stringBuilder = new StringBuilder(substring);
+        stringBuilder.reverse();
+        System.out.println(string.replaceAll(substring, stringBuilder.toString()));
 
     }
 
     public static String getWordsReverse(String revereString) {
-      return   Arrays.stream(revereString.split(" "))
-              .map(string -> new StringBuilder(string).reverse().toString())
-                .collect(Collectors.joining(" "));
+        return String.join(" ", Arrays.asList(revereString.split(" ")).reversed());
     }
 
-    public static void getWordsReverseInColumn(String string) {
-        String[] split = string.split(" ");
-        for (String strings : split) {
-            StringBuilder stringBuilder = new StringBuilder(strings);
-            StringBuilder reverse = stringBuilder.reverse();
-            System.out.print(reverse + " ");
-        }
+    public static String getWordsReverseInColumn(String string) {
+        return Arrays.stream(string.split(" "))
+                .map(StringBuilder::new)
+                .map(StringBuilder::reverse)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
+
 
